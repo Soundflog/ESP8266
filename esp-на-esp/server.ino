0d3df890b393f2bb64deb8368e4a50e1ee82eab6
Pablo <pablo-ubica696@yandex.ru>
2024-03-07 12:13:13 +0000

    update esp-на-esp/server.ino

--- esp-на-esp/server.ino
+++ esp-на-esp/server.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "Martin";
-const char* password = "987651234";
+const char* ssid = "WIFINAme";
+const char* password = "PasswordWifi";
 
 ESP8266WebServer server(80);
 
